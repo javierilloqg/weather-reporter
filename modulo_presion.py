d9d1add9df7e46f4a88574c832f78e0dd13fb82f
Javier Quesada Galbán <javiquesadagalban@gmail.com>
2021-09-15 20:14:11 +0200

    Add header block

--- modulo_presion.py
+++ modulo_presion.py
@@ -1,3 +1,18 @@
+#******************************************************************************
+#* Copyright (C) 2020 by Javier Quesada Galban
+#*
+#* Python module to show possible storms
+#*
+#******************************************************************************
+#
+#* @file modulo_presion.py
+#* @author Javier Quesada Galban
+#* @date 13 April 2021
+#* @brief This module will return information about possible storms based on 
+#  barometric preassure from openweather data 
+#*
+#
+
 def comprobarpresion(presion):
     if presion <= 990:
         return("¡Posibles tormentas!")
